9f295f5c013d4293973121ec55ca0dce5cefdf50
MD AMJAD RAZA <123620419+amjadraza19@users.noreply.github.com>
2024-05-26 19:25:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     
      <div className='w-full h-screen flex flex-col backgroud relative overflow-x-hidden items-center'>
-      <h1 className='absolute bg-blue-500 rounded-lg w-11/12 text-center mt-[40px] px-10 py-2 text-3xl font-bold'>RANDOM GIFs (MEMEs R)</h1>
+      <h1 className='absolute bg-blue-500 rounded-lg w-11/12 text-center mt-[40px] px-10 py-2 h-20 text-3xl font-bold'>RANDOM GIFs (MEMEs R)</h1>
       <div className='flex flex-col w-full items-center gap-y-10 mt-[30px]'>
         <Random />
         <Tag />
